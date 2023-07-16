f450d3a4e2834154778840aefb49649566009b43
Javad <mmkismylove@gmail.com>
2023-07-16 18:36:35 +0200

    gitthingi

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: 'localhost',
-    port: 3005
-  }
+  base:"tailweb1"
+  
 })
